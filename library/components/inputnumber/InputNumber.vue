ef11f66fd75b0244c3e392b2aadf4760d649fade
Priya Bagus Amanullah <priyabagus.a29@gmail.com>
2024-08-13 11:02:31 +0700

    feat: add component InputNumber

---
+++ library/components/inputnumber/InputNumber.vue
@@ -0,0 +1,275 @@
+<script setup lang="ts">
+import { computed, nextTick, onMounted, reactive, ref, watch } from 'vue';
+import { useField } from 'vee-validate';
+import { FieldValidation } from 'lib/types/validation.type';
+import type { InputNumberProps, InputNumberEmits } from './InputNumber.vue.d';
+
+import InputNumber, { InputNumberInputEvent } from 'primevue/inputnumber';
+import FieldWrapper from '../fieldwrapper/FieldWrapper.vue';
+import ValidatorMessage from '../validatormessage/ValidatorMessage.vue';
+
+import InputGroup from '../inputgroup/InputGroup.vue';
+import InputGroupAddon from 'primevue/inputgroupaddon';
+import Icon from '../icon/Icon.vue';
+
+const props = withDefaults(defineProps<InputNumberProps>(), {
+  maxDigit: 16,
+  padStart: 0,
+  showValidatorMessage: true,
+});
+
+const emit = defineEmits<InputNumberEmits>();
+
+const inputKey = ref<number>(0);
+
+const field = reactive<FieldValidation<number | undefined>>({
+  value: props.value ?? props.modelValue,
+});
+
+const inputPlaceholder = computed(
+  () =>
+    props.placeholder ??
+    `Enter ${props.label ? props.label.toLowerCase() : 'number'}`,
+);
+
+onMounted(() => {
+  if (props.useValidator) {
+    Object.assign(
+      field,
+      useField(
+        props.fieldName ?? 'numberInput',
+        (value: number | undefined) => {
+          return setValidatorMessage(value);
+        },
+      ),
+    );
+
+    // Check if props.value is not null or undefined
+    // eslint-disable-next-line eqeqeq
+    if (props.value != null) field.value = props.value;
+  }
+});
+
+const invalidInput = computed(() => props.invalid || !!field.errorMessage);
+const inputNumberId = computed(() => {
+  if (props.inputId) return props.inputId;
+
+  return (
+    (props.label ?? props.fieldName ?? 'input-number')
+      .toLowerCase()
+      .trim()
+      .replaceAll(' ', '-') + '-inputnumber'
+  );
+});
+
+const setValidatorMessage = async (
+  value: number | undefined,
+): Promise<string | boolean> => {
+  await nextTick();
+
+  // eslint-disable-next-line eqeqeq
+  if (props.invalid && typeof props.validatorMessage === 'string') {
+    return props.validatorMessage;
+  } else if (typeof props.validatorMessage !== 'string') {
+    const { empty } = props.validatorMessage ?? {};
+    if (!value && props.mandatory) {
+      return empty ?? true;
+    }
+  }
+
+  return true;
+};
+
+const onUpdateValue = (
+  event: InputNumberInputEvent | number,
+  type: 'modelValue' | 'input',
+): void => {
+  const eventValue =
+    type === 'input' ? (event as InputNumberInputEvent).value : event;
+
+  const assignEventValue = (): number | string => {
+    if (eventValue != null) {
+      return eventValue !== '-' ? +eventValue : eventValue;
+    }
+
+    return undefined;
+  };
+
+  let value = assignEventValue();
+
+  // eslint-disable-next-line eqeqeq
+  if (value != null && !isNaN(+value)) {
+    // eslint-disable-next-line eqeqeq
+    value = eventValue != null ? +eventValue : undefined;
+    handleNumberValue(value, type);
+    // eslint-disable-next-line eqeqeq
+  } else if (value == null && typeof value !== 'string') {
+    field.value = value;
+
+    if (type === 'input') {
+      emit('input', undefined);
+    } else {
+      emit('update:modelValue', undefined);
+    }
+  }
+};
+
+const handleNumberValue = (
+  value: number,
+  type: 'modelValue' | 'input',
+): void => {
+  const isExceed = typeof value === 'number' ? isExceedMaxDigits(value) : false;
+
+  let slicedValue: number | string | undefined = value
+    ?.toString()
+    .slice(0, value < 0 ? props.maxDigit + 1 : props.maxDigit);
+
+  // eslint-disable-next-line eqeqeq
+  if (slicedValue != null) {
+    slicedValue = +slicedValue;
+  }
+
+  field.value = isExceed ? +slicedValue : (value as number | undefined);
+
+  if (isExceed) inputKey.value++;
+
+  if (type === 'input') {
+    emit('input', isExceed ? +slicedValue : value);
+  } else {
+    emit('update:modelValue', isExceed ? +slicedValue : value);
+  }
+};
+
+const isExceedMaxDigits = (value: number | undefined): boolean => {
+  return (
+    // eslint-disable-next-line eqeqeq
+    value != null &&
+    value.toString().length > (props.maxDigit > 16 ? 16 : props.maxDigit)
+  );
+};
+
+const increment = (): void => {
+  onUpdateValue(+(field.value ?? 0) + 1, 'modelValue');
+};
+
+const decrement = (): void => {
+  onUpdateValue(+(field.value ?? 0) - 1, 'modelValue');
+};
+
+/**
+ * We need to watch the props.value changes for async data only once after it rendered.
+ */
+watch(
+  () => props.value,
+  (value) => {
+    if (!isExceedMaxDigits(value)) {
+      field.value = value;
+    }
+  },
+  { once: true },
+);
+
+watch(
+  () => props.modelValue,
+  (model) => {
+    if (!isExceedMaxDigits(model)) {
+      field.value = model;
+    }
+  },
+);
+</script>
+<template>
+  <FieldWrapper
+    :info="fieldInfo"
+    :label="props.label"
+    :mandatory="props.mandatory"
+  >
+    <InputGroup
+      :class="[
+        'grid ',
+        {
+          'grid-cols-[max-content,1fr,max-content]':
+            ($slots['addon-left'] && $slots['addon-right']) || showButtons,
+          'grid-cols-[1fr,max-content]':
+            !$slots['addon-left'] && $slots['addon-right'],
+          'grid-cols-[max-content,1fr]':
+            $slots['addon-left'] && !$slots['addon-right'],
+        },
+        { 'w-max': autoResize },
+        inputGroupClass,
+      ]"
+      :disabled="props.disabled"
+      :invalid="invalidInput"
+    >
+      <InputGroupAddon
+        v-if="$slots['addon-left'] || props.showButtons"
+        :class="[
+          {
+            '!px-2': props.showButtons,
+          },
+          addonLeftClass,
+        ]"
+        :disabled="disabled"
+      >
+        <slot name="addon-left">
+          <Icon
+            @click="decrement"
+            class="!text-base text-general-500"
+            icon="subtract"
+          />
+        </slot>
+      </InputGroupAddon>
+
+      <span
+        :class="['relative', { 'w-max': autoResize }]"
+        data-ts-section="autoresizeinputwrapper"
+      >
+        <span
+          v-if="autoResize"
+          class="text-body-medium !tracking-[0.28px] user-select-none py-1 px-3 inline-block min-w-4 leading-[17px]"
+          data-ts-section="inputresizer"
+        >
+          {{ (field.value ?? '').toString().padStart(padStart, '0') }}
+        </span>
+
+        <InputNumber
+          :key="inputKey"
+          v-bind="$props"
+          :class="[
+            inputNumberClass,
+            {
+              'absolute left-0 top-0 !text-transparent caret-general-800':
+                autoResize,
+            },
+          ]"
+          :input-id="inputNumberId"
+          :model-value="field.value"
+          :placeholder="autoResize ? '' : inputPlaceholder"
+          :show-buttons="false"
+          @input="onUpdateValue($event, 'input')"
+          @update:model-value="onUpdateValue($event, 'modelValue')"
+        />
+      </span>
+
+      <InputGroupAddon
+        v-if="$slots['addon-right'] || props.showButtons"
+        :class="{
+          '!text-general-200 !dark:text-general-200': props.disabled,
+          '!px-2': props.showButtons,
+        }"
+      >
+        <slot name="addon-right">
+          <Icon
+            @click="increment"
+            class="!text-base text-general-500"
+            icon="add"
+          />
+        </slot>
+      </InputGroupAddon>
+    </InputGroup>
+    <ValidatorMessage
+      v-if="props.showValidatorMessage"
+      :message="field.errorMessage"
+    />
+  </FieldWrapper>
+</template>
