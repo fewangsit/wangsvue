ef73c1f9e83164870a5276bc25e66de660a66e71
Priya Bagus Amanullah <priyabagus.a29@gmail.com>
2024-08-13 15:48:28 +0700

    refactor: change ring color for inputgroup

--- library/preset/inputgroup/index.js
+++ library/preset/inputgroup/index.js
@@ -3,7 +3,7 @@ export default {
     class: [
       'flex items-stretch',
       'w-full',
-      'ring-inset ring-general-200 dark:ring-grayscale-600 rounded',
+      'ring-inset ring-general-400 dark:ring-grayscale-600 rounded',
       'bg-white dark:bg-grayscale-900',
     ],
   },
