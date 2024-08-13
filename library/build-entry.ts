e2c2b36059901b1ed14057ba7fbb294182a8f03c
zainkurnia_ <programmer-fe3@qwertysystem.net>
2024-08-13 10:44:50 +0700

    build: export some components

--- library/build-entry.ts
+++ library/build-entry.ts
@@ -57,12 +57,23 @@ import {
   Badge,
   BadgeGroup,
   Button,
+  ButtonBulkAction,
   Breadcrumb,
   Card,
   DataTable,
   TabMenu,
   Toast,
   UserName,
+  Icon,
+  ButtonDownload,
+  ButtonFilter,
+  FilterContainer,
+  DialogConfirm,
+  Checkbox,
+  Menu,
+  ButtonSearch,
+  InputRangeNumber,
+  Calendar,
 } from './components';
 
 export default {
@@ -76,11 +87,21 @@ export default {
 
     app.component('Badge', Badge);
     app.component('BadgeGroup', BadgeGroup);
-    // eslint-disable-next-line vue/no-reserved-component-names
     app.component('Button', Button);
+    app.component('ButtonBulkAction', ButtonBulkAction);
+    app.component('ButtonDownload', ButtonDownload);
+    app.component('ButtonFilter', ButtonFilter);
+    app.component('FilterContainer', FilterContainer);
+    app.component('DialogConfirm', DialogConfirm);
+    app.component('Checkbox', Checkbox);
+    app.component('Menu', Menu);
+    app.component('ButtonSearch', ButtonSearch);
+    app.component('Calendar', Calendar);
+    app.component('InputRangeNumber', InputRangeNumber);
     app.component('Breadcrumb', Breadcrumb);
     app.component('Card', Card);
     app.component('DataTable', DataTable);
+    app.component('Icon', Icon);
     app.component('TabMenu', TabMenu);
     app.component('Toast', Toast);
     app.component('UserName', UserName);
@@ -111,6 +132,7 @@ export {
   Breadcrumb,
   Card,
   DataTable,
+  Icon,
   TabMenu,
   Toast,
   UserName,
