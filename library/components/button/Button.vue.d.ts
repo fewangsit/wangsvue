b9e29f8d0d552ad6662919f4f7ec1f40e5eef844
zainkurnia_ <programmer-fe3@qwertysystem.net>
2024-08-13 13:08:53 +0700

    revert changes on button d.ts

--- library/components/button/Button.vue.d.ts
+++ library/components/button/Button.vue.d.ts
@@ -1,15 +1,9 @@
-import { ButtonHTMLAttributes, DefineComponent } from 'vue';
+import { DefineComponent } from 'vue';
+import { ButtonProps as PrimeButtonProps } from 'primevue/button';
 import type { WangsIcons } from 'lib/components/icon/Icon.vue.d';
 
-export interface ButtonProps extends ButtonHTMLAttributes {
-  /**
-   * Text of the button.
-   */
-  label?: string | undefined;
-  /**
-   * Defines the style of the button.
-   */
-  severity?: 'secondary' | 'success' | 'warning' | 'danger';
+export interface ButtonProps
+  extends /* @vue-ignore */ Omit<PrimeButtonProps, 'icon' | 'iconClass'> {
   /**
    * Tooltip/information to show on button hover.
    */
@@ -28,25 +22,6 @@ export interface ButtonProps extends ButtonHTMLAttributes {
    * Style class of the icon.
    */
   iconClass?: string | string[];
-  /**
-   * Add a circular border radius to the button.
-   * @defaultValue false
-   */
-  rounded?: boolean | undefined;
-  /**
-   * Add a textual class to the button without a background initially.
-   * @defaultValue false
-   */
-  text?: boolean | undefined;
-  /**
-   * Add a border class without a background initially.
-   * @defaultValue false
-   */
-  outlined?: boolean | undefined;
-  /**
-   * Defines the size of the button.
-   */
-  size?: 'small' | 'large' | undefined;
 }
 
 declare const TSButton: DefineComponent<ButtonProps>;
