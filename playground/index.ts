68e17298fdbf6943d0ac34f560256652a2b2a2c3
zainkurnia_ <programmer-fe3@qwertysystem.net>
2024-08-26 09:05:05 +0700

    comment out a route

--- playground/index.ts
+++ playground/index.ts
@@ -12,12 +12,14 @@ const routes: Readonly<RouteRecordRaw[]> = [
         component: (): Promise<Component> =>
           import('./docs/datatable/TreeTableDocs.vue'),
       },
-      {
-        path: 'imagecompressor',
-        name: 'Image Docs',
-        component: (): Promise<Component> =>
-          import('./docs/image/ImageCompressorDocs.vue'),
-      },
+      /*
+       * {
+       *   path: 'imagecompressor',
+       *   name: 'Image Docs',
+       *   component: (): Promise<Component> =>
+       *     import('./docs/image/ImageCompressorDocs.vue'),
+       * },
+       */
       /*
        * {
        *   path: 'button',
