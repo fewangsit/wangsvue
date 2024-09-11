513038db8f14a44e2c90cc978e7b66bf40d7c2f0
programmer-fe6 <145011686+programmer-fe6@users.noreply.github.com>
2024-09-11 11:13:15 +0700

    feat(litedropdown): added new component litedropdown
    This component removes the unnecessary bloat that comes with the regular dropdown
    - No label
    - No internal validator message
    - Unique styling, different from regular dropdown
    - No right or left addon
    - And many more...

---
+++ library/components/litedropdown/LiteDropdown.vue.d.ts
@@ -0,0 +1,96 @@
+import { DropdownOption, OptionValue } from 'lib/types/options.type';
+import { ClassComponent, GlobalComponentConstructor } from '../ts-helpers';
+import { DropdownChangeEvent } from 'primevue/dropdown';
+
+/**
+ * Lite dropdown component props
+ */
+export interface LiteDropdownProps {
+  modelValue: OptionValue;
+  options: DropdownOption[];
+  /**
+   * Specify the property name of option to be used as label.
+   *
+   * @default undefined - the label will be sets to the option itself.
+   */
+  optionLabel: string;
+  /**
+   * Specify the property name of option to be used as value.
+   *
+   * @default undefined - the value will be sets to the option itself.
+   */
+  optionValue: string;
+  /**
+   * Determines if the field is mandatory
+   */
+  mandatory?: boolean;
+  /**
+   * Default text to display when no option is selected.
+   *
+   * @default `Pilih data`
+   */
+  placeholder?: string;
+  /**
+   * Whether the dropdown is in loading state.
+   * @defaultValue false
+   */
+  loading?: boolean;
+  /**
+   * Set disabled state for input dropdown.
+   */
+  disabled?: boolean;
+  /**
+   * Wether show the Dropdown option search or not.
+   *
+   * @default true,
+   */
+  filter?: boolean;
+  /**
+   * The filter input Placeholder
+   *
+   * @default 'Cari'
+   */
+  filterPlaceholder?: string;
+}
+
+/**
+ * Lite dropdown component emits
+ */
+export type LiteDropdownEmits = {
+  /**
+   * Emits when an option selected.
+   */
+  'update:modelValue': [value: OptionValue | undefined];
+  /**
+   * Emits when overlay shown.
+   */
+  'show': [];
+  /**
+   * Emits when there is a change on the dropdown value.
+   */
+  'change': [value: DropdownChangeEvent];
+};
+
+/**
+ * **WangsVue - LiteDropwdown**
+ *
+ * _LiteDropdown, a lite version of the Dropdown component._
+ *
+ * --- ---
+ * ![WangsVue](https://ik.imagekit.io/kurniadev/TS-HEAD-BLACK.png)
+ *
+ * @group Component
+ */
+declare class LiteDropdown extends ClassComponent<
+  LiteDropdownProps,
+  unknown,
+  LiteDropdownEmits
+> {}
+
+declare module '@vue/runtime-core' {
+  interface GlobalComponents {
+    Dropdown: GlobalComponentConstructor<LiteDropdown>;
+  }
+}
+
+export default LiteDropdown;
