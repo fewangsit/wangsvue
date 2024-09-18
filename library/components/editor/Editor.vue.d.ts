9b8c81a9e4f1c0281af8934f13a71d00c03e4501
programmer-mob5 <programmer-mob5@qwertysystem.net>
2024-09-18 13:51:04 +0700

    feat : Dialog Upload Image

---
+++ library/components/editor/Editor.vue.d.ts
@@ -0,0 +1,46 @@
+import { ClassComponent } from '../ts-helpers';
+
+export interface EditorProps {
+  /**
+   * Initial Value For Content
+   */
+  textContentInitialValue: JSONContent;
+  /**
+   * Model Value For Editor Content
+   */
+  modelValue: JSONContent;
+}
+
+export type EditorEmits = {
+  /**
+   * Event emitted when the model value is updated
+   */
+  'update:modelValue': [value: JSONContent];
+  /**
+   * Event emitted when the upload image from local file
+   */
+  'postImageLocal': [value: PostImage];
+};
+
+export type PostImage = {
+  image: File;
+  setImageCb: (imageUrl: string) => void;
+};
+
+/**
+ * Editor Content Meta Data
+ */
+export type JSONContent = {
+  type?: string;
+  attrs?: Record<string, any>;
+  content?: JSONContent[];
+  marks?: {
+    type: string;
+    attrs?: Record<string, any>;
+    [key: string]: any;
+  }[];
+  text?: string;
+  [key: string]: any;
+};
+
+declare class Dropdown extends ClassComponent<EditorProps, unknown, unknown> {}
--- playground/docs/editor/EditorDocs.vue
+++ playground/docs/editor/EditorDocs.vue
@@ -2,6 +2,50 @@
 import Card from 'lib/components/card/Card.vue';
 import DocTitle from '../DocTitle.vue';
 import Editor from 'lib/components/editor/Editor.vue';
+import { ref } from 'vue';
+import { PostImage } from 'lib/components/editor/Editor.vue.d';
+
+const editorContent = ref();
+
+const data = {
+  type: 'doc',
+  content: [
+    {
+      type: 'paragraph',
+      content: [
+        {
+          type: 'text',
+          text: 'asdsadasd',
+        },
+      ],
+    },
+    {
+      type: 'paragraph',
+      content: [
+        {
+          type: 'text',
+          text: 'asdasd',
+        },
+      ],
+    },
+    {
+      type: 'paragraph',
+      content: [
+        {
+          type: 'text',
+          text: 'sad',
+        },
+      ],
+    },
+    {
+      type: 'paragraph',
+    },
+  ],
+};
+
+const sendImage = (value: PostImage): void => {
+  value.setImageCb('https://www.svgrepo.com/show/452030/avatar-default.svg');
+};
 </script>
 
 <template>
@@ -10,7 +54,11 @@ import Editor from 'lib/components/editor/Editor.vue';
       <DocTitle name="Editor" />
     </template>
     <template #content>
-      <Editor />
+      <Editor
+        v-model:model-value="editorContent"
+        :text-content-initial-value="data"
+        @post-image-local="sendImage"
+      />
     </template>
   </Card>
 </template>
