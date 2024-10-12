a48419319a55c14a8096b09b7e730b3b502aa2fc
Rhapsodya Piedro Asmorobangun <piedrorhapsodya@gmail.com>
2024-10-12 12:00:21 +0700

    refactor(InputGroup): inject Preset

--- library/components/inputgroup/InputGroup.vue
+++ library/components/inputgroup/InputGroup.vue
@@ -1,14 +1,12 @@
 <script setup lang="ts">
-import InputGroup from 'primevue/inputgroup';
+import { inject } from 'vue';
 import { InputGroupProps } from './InputGroup.vue.d';
+
+import InputGroup from 'primevue/inputgroup';
 
 defineProps<InputGroupProps>();
 
-/**
- * To set different ring width for Firefox Browser
- * Also for InputGroupAddon
- */
-const isFirefoxBased = navigator.userAgent.includes('Firefox');
+const Preset = inject<Record<string, any>>('preset', {}).inputgroup;
 
 const focusToInput = (e: MouseEvent): void => {
   const target = e.target as HTMLElement;
@@ -22,28 +20,9 @@ const focusToInput = (e: MouseEvent): void => {
 
 <template>
   <InputGroup
-    :class="[
-      {
-        '!ring-danger-500 [&_.inputgroupaddon]:ring-danger-500':
-          $props.invalid && ring != 'none',
-        '[&_.inputgroupaddon]:ring-general-400':
-          !$props.invalid && ring != 'none',
-        '!bg-general-50 ': $props.disabled,
-      },
-      {
-        '[&:has(:focus)]:!ring-primary-400 [&:has(:focus)_.inputgroupaddon]:!ring-primary-400':
-          ring != 'none',
-      },
-      {
-        'ring-[1px] [&_.inputgroupaddon]:ring-[1px]':
-          isFirefoxBased && ring != 'none',
-        'ring-[0.5px] [&_.inputgroupaddon]:ring-[0.5px]':
-          !isFirefoxBased && ring != 'none',
-      },
-      '[&:has(input)]:cursor-text',
-      'h-[30px]',
-      $attrs['class'],
-    ]"
+    :pt="{
+      root: Preset.root({ props: { ...$props, class: $attrs['class'] } }),
+    }"
     @click="focusToInput"
   >
     <slot />
--- presets/wangsvue/inputgroup/index.js
+++ presets/wangsvue/inputgroup/index.js
@@ -1,10 +1,37 @@
+/**
+ * To set different ring width for Firefox Browser
+ * Also for InputGroupAddon
+ */
+const isFirefoxBased = navigator.userAgent.includes('Firefox');
+
 export default {
-  root: {
-    class: [
+  root: ({ props }) => ({
+    'class': [
+      {
+        '!ring-danger-500 [&_.inputgroupaddon]:ring-danger-500':
+          props.invalid && props.ring != 'none',
+        '[&_.inputgroupaddon]:ring-general-400':
+          !props.invalid && props.ring != 'none',
+        '!bg-general-50 ': props.disabled,
+      },
+      {
+        '[&:has(:focus)]:!ring-primary-400 [&:has(:focus)_.inputgroupaddon]:!ring-primary-400':
+          props.ring != 'none',
+      },
+      {
+        'ring-[1px] [&_.inputgroupaddon]:ring-[1px]':
+          isFirefoxBased && props.ring != 'none',
+        'ring-[0.5px] [&_.inputgroupaddon]:ring-[0.5px]':
+          !isFirefoxBased && props.ring != 'none',
+      },
+      '[&:has(input)]:cursor-text',
+      'h-[30px]',
       'flex items-stretch',
       'w-full',
       'ring-inset ring-general-400 rounded',
       'bg-white',
+      props.class,
     ],
-  },
+    'data-wv-section': 'input-group',
+  }),
 };
