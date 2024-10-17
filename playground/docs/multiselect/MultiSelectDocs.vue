ca3d6bcae30f5a45faf765b6ae891ec30d3cb639
Zain Kurnia <programmer-fe3@qwertysystem.net>
2024-10-17 11:05:16 +0700

    fix(MultiSelect): assign props initial value or modelValue as initialValue of field.value

--- playground/docs/multiselect/MultiSelectDocs.vue
+++ playground/docs/multiselect/MultiSelectDocs.vue
@@ -7,9 +7,90 @@ import MultiSelect from 'lib/components/multiselect/MultiSelect.vue';
 import Button from 'lib/components/button/Button.vue';
 
 const model = ref<OptionValue[]>();
+const objectValueModel = ref<OptionValue[]>([
+  { _id: '1', status: 'Waiting for Approval' },
+]);
 const items = ref(
   Array.from({ length: 100000 }, (_, i) => ({ label: `Item #${i}`, value: i })),
 );
+
+const selectedProcess = {
+  _id: '67106aecb1cbb5ebd3760575',
+  name: 'hhhhhh',
+  team: [{ _id: '66beef9f787cdd8139a71515', teamInitial: 'UIUX' }],
+  processPosition: 'Di luar Deployment',
+  reviewLeader: true,
+  active: true,
+  nonactivable: false,
+  default: false,
+  dependencies: [
+    { _id: '6707a73a2492eb56557b6bab', name: 'Repositori BE' },
+    {
+      _id: '6707a73a2492eb56557b6bac',
+      name: 'Repositori FE',
+    },
+  ],
+  parentsOf: [],
+  createdAt: '2024-10-17 01:39:56',
+};
+
+const dependenciesOptions = [
+  { _id: '6707a73a2492eb56557b6bab', name: 'Repositori BE' },
+  {
+    _id: '6707a73a2492eb56557b6bac',
+    name: 'Repositori FE',
+  },
+  { _id: '6707a73a2492eb56557b6bad', name: 'Repositori Mobile' },
+  {
+    _id: '6707a73a2492eb56557b6bae',
+    name: 'Pengonsepan',
+  },
+  { _id: '6707a73a2492eb56557b6baf', name: 'Diagram' },
+  {
+    _id: '6707a73a2492eb56557b6bb0',
+    name: 'Wireframe',
+  },
+  { _id: '6707a73a2492eb56557b6bb1', name: 'Komponen Web' },
+  {
+    _id: '6707a73a2492eb56557b6bb2',
+    name: 'Komponen Mobile',
+  },
+  { _id: '6707a73a2492eb56557b6bb3', name: 'API Spec' },
+  {
+    _id: '6707a73a2492eb56557b6bb4',
+    name: 'Database',
+  },
+  { _id: '6707a73a2492eb56557b6bb5', name: 'Create API' },
+  {
+    _id: '6707a73a2492eb56557b6bb6',
+    name: 'Slicing Komponen Web',
+  },
+  { _id: '6707a73a2492eb56557b6bb7', name: 'Slicing Web' },
+  {
+    _id: '6707a73a2492eb56557b6bb8',
+    name: 'Slicing Komponen Mobile',
+  },
+  { _id: '6707a73a2492eb56557b6bb9', name: 'Slicing Mobile' },
+  {
+    _id: '6707a73a2492eb56557b6bba',
+    name: 'IOT',
+  },
+  { _id: '6707a73a2492eb56557b6bbb', name: 'Dokumentasi API' },
+  {
+    _id: '6707a73a2492eb56557b6bbc',
+    name: 'User Manual Web',
+  },
+  { _id: '6707a73a2492eb56557b6bbd', name: 'User Manual Mobile' },
+  {
+    _id: '6707a73a2492eb56557b6bbe',
+    name: 'Detailing',
+  },
+  { _id: '6707a73a2492eb56557b6bbf', name: 'Connect API Web' },
+  {
+    _id: '6707a73a2492eb56557b6bc0',
+    name: 'Connect API Mobile',
+  },
+];
 </script>
 
 <template>
@@ -32,10 +113,34 @@ const items = ref(
             placeholder="Pilih status"
           />
           <MultiSelect
-            v-model="model"
+            v-model="objectValueModel"
             :invalid="true"
-            :options="['Waiting for Approval', 'Waiting for Handover']"
+            :options="[
+              { _id: '1', status: 'Waiting for Approval' },
+              { _id: '2', status: 'Waiting for Handover' },
+            ]"
             label="Status"
+            option-label="status"
+          />
+
+          {{ selectedProcess?.dependencies }}
+          <hr />
+          {{
+            dependenciesOptions.filter((option) =>
+              selectedProcess?.dependencies.some(
+                (dep) => dep.name === option.name,
+              ),
+            )
+          }}
+          <MultiSelect
+            :disabled="!!selectedProcess?.default"
+            :initial-value="selectedProcess?.dependencies"
+            :options="dependenciesOptions"
+            data-key="_id"
+            field-name="dependencies"
+            label="Dependensi"
+            option-label="name"
+            use-validator
           />
 
           <Button @click="model = undefined" label="Empty Model" />
