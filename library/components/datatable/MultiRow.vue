fa15d4cfe44f056bac931dc49046dfc0ebc889e3
Priya Bagus Amanullah <priyabagus.a29@gmail.com>
2024-10-25 16:42:01 +0700

    feat(Preset): add class preset for multirow in datatable preset

--- library/components/datatable/MultiRow.vue
+++ library/components/datatable/MultiRow.vue
@@ -1,16 +1,25 @@
 <script setup lang="ts">
-defineProps<{
+import { inject } from 'vue';
+
+const props = defineProps<{
   values: string[];
 }>();
+
+const Preset = inject<Record<string, any>>('preset', {}).datatable;
 </script>
 
 <template>
-  <div :class="`grid-rows-${values.length}`" class="grid grid-cols-1">
+  <div v-bind="Preset?.multirowcontainer({ props })">
     <span
       :key="index"
       v-for="(value, index) in values"
-      :class="{ '!border-t border-primary-100 !border-solid': index !== 0 }"
-      class="!h-[35px] flex items-center px-2"
+      v-bind="
+        Preset?.multirow({
+          context: {
+            firstIndex: index === 0,
+          },
+        })
+      "
     >
       {{ value }}
     </span>
