63c84affa4fdf09789400260dd73755f81a262e1
Priya Bagus Amanullah <priyabagus.a29@gmail.com>
2024-10-25 16:27:52 +0700

    feat(MultiRow in DataTable): add preset multirow
    -Add new component for DataTable to render multirow preset
    -Adjust declaration type file for DataTable
    -Add documentation to use multirow preset

---
+++ library/components/datatable/MultiRow.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+defineProps<{
+  values: string[];
+}>();
+</script>
+
+<template>
+  <div :class="`grid-rows-${values.length}`" class="grid grid-cols-1">
+    <span
+      :key="index"
+      v-for="(value, index) in values"
+      :class="{ '!border-t border-primary-100 !border-solid': index !== 0 }"
+      class="!h-[35px] flex items-center px-2"
+    >
+      {{ value }}
+    </span>
+  </div>
+</template>
