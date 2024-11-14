38697e77c1b4c995f5fc3cb2d89183b9cecf56d2
dzulqtera <dzul@qtera.co.id>
2024-11-14 17:29:16 +0700

    feat(tasktable): add table actions for restore and delete permanently

---
+++ library/components/tasktable/DialogConfirmDeleteTaskPermanently.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import DialogConfirm from 'lib/components/dialogconfirm/DialogConfirm.vue';
+import { TaskDetailData } from 'lib/types/task.type';
+
+const visible = defineModel<boolean>('visible', { required: true });
+
+const props = defineProps<{
+  tasks: Pick<TaskDetailData, '_id' | 'name'>[];
+}>();
+</script>
+
+<template>
+  <DialogConfirm
+    v-model:visible="visible"
+    :list="props.tasks?.map((task) => task?.name)"
+    confirm-label="Hapus"
+    header="Hapus Permanen"
+    message="Jika kamu menghapus data ini, kamu tidak akan dapat memulihkannya. Apakah kamu yakin ingin menghapusnya?"
+    severity="danger"
+  />
+</template>
---
+++ library/components/tasktable/DialogConfirmRestoreTask.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import DialogConfirm from 'lib/components/dialogconfirm/DialogConfirm.vue';
+
+const visible = defineModel<boolean>('visible', { required: true });
+</script>
+
+<template>
+  <DialogConfirm
+    v-model:visible="visible"
+    confirm-label="Simpan"
+    header="Pulihkan Task"
+    message="Task ini akan kembali menjadi aktif dan berstatus backlog. Apakah kamu yakin ingin memulihkannya?"
+    severity="success"
+  />
+</template>
