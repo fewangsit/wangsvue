1bc3cb4f7a235370a8b9669615c379f356d6c145
Rhapsodya Piedro Asmorobangun <piedrorhapsodya@gmail.com>
2024-12-30 14:38:30 +0700

    feat(notification): PBI, sprint notifications

--- library/components/notificationitem/helpers/goToDetail.helper.ts
+++ library/components/notificationitem/helpers/goToDetail.helper.ts
@@ -18,11 +18,17 @@ export const goToAccountMember = (notification: NotificationItemType): void => {
 
 export const goToQC = (notification: NotificationItemType): void => {
   sessionStorage.setItem('projectId', notification.data.projectId as string);
-  sessionStorage.setItem(
-    'subModuleId',
-    notification.data.subModuleId as string,
-  );
-  const url = `/proyek/detail-proyek/quality-control/e2e-testing/pending-e2e-testing-${notification.data.platform}/end-to-end-testing`;
+  let url: string;
+  if (notification.data.subModuleId) {
+    sessionStorage.setItem(
+      'subModuleId',
+      notification.data.subModuleId as string,
+    );
+    url = `/proyek/detail-proyek/quality-control/e2e-testing/pending-e2e-testing-${notification.data.platform}/end-to-end-testing`;
+  } else if (notification.data.pbiId) {
+    sessionStorage.setItem('pbiId', notification.data.pbiId);
+    url = `/proyek/detail-proyek/quality-control/pbi-testing/${notification.data.platform}`;
+  }
   navigateToUrl(url);
   if (window.location.pathname.includes(url)) window.location.reload();
 };
@@ -34,7 +40,21 @@ export const goToProject = (notification: NotificationItemType): boolean => {
       `/ticket/task-ticket/detail-ticket/${notification.data.ticketId}`,
     );
   else if (notification.data.projectId) {
+    if (notification.data.taskId) return true;
     sessionStorage.setItem('projectId', notification.data.projectId);
+    if (notification.data.sprintId) {
+      url = `/proyek/detail-proyek/sprint/sprint/${notification.data.sprintId}/detail-sprint`;
+      navigateToUrl(url);
+      return false;
+    }
+    if (notification.data.pbiId) {
+      sessionStorage.setItem('pbiId', notification.data.pbiId);
+      url = '/proyek/detail-proyek/sprint';
+      if (notification.module === 'Comment')
+        sessionStorage.setItem('openComment', notification.data.pbiId);
+      navigateToUrl(url);
+      return false;
+    }
     if (notification.data.moduleId) {
       url = `/proyek/detail-proyek/modul/${notification.data.moduleId}/detail-modul`;
       if (notification.module === 'Comment') url += '/komentar';
@@ -47,7 +67,6 @@ export const goToProject = (notification: NotificationItemType): boolean => {
       navigateToUrl(url);
       return false;
     }
-    if (notification.data.taskId) return true;
     url = '/proyek/detail-proyek/tim';
     navigateToUrl(url);
     if (window.location.pathname.includes(url)) window.location.reload();
